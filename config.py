300fb873145c3fb14c3c3bfc00fb5fa88a48f4a5
dertamme <tammeras@outlook.com>
2025-10-17 10:57:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ STATUS_FILE_DIRECTORY = input_ordner
 text_passages_ordner = "text_passages/"
 relevant_text_passages_ordner =  "text_passages/relevant_text_passages/"
 analyse_ordner = "text_passages/analyse/"
+aussagen_alle_jahre_ornder = "matching/aussagen"
 gemini_model_version = "gemini-2.5-flash"
